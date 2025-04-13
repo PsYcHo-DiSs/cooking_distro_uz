e3433d12ca9e67b3886bb4a5af34ab507270354a
Artem Lobzov <psychodiss@gmail.com>
2025-04-13 17:56:08 +0300

    made more wide admin options to view customized admin panel with django-jazzmin added mini photo of post functional to view, filters

--- cooking/admin.py
+++ cooking/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'watched', 'is_published', 'category', 'created_at', 'updated_at', 'get_photo_image')
+    list_display_links = ('id', 'title')
+    list_editable = ('is_published',)
+    readonly_fields = ('watched',)
+    list_filter = ('is_published', 'category', 'watched',)
+
+    def get_photo_image(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50" height="60">')
+        return "-"
+    get_photo_image.short_description = 'Изображение поста'
+
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
